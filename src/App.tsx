96b0a6e407b9675c808b79095d2b50c518c986ce
Thomas Murzyn <murzynthomas@gmail.com>
2023-02-19 12:19:07 +0100

    Add a timer in app

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,31 @@ import { useAppSelector, useAppDispatch } from "./app/hooks";
 import Button from "./components/button/button.component";
 import { ButtonType } from "./components/button/button.component";
 import { resetGame } from "./app/features/game/game.slice";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const points = useAppSelector((state) => state.game.points);
   const dispatch = useAppDispatch();
+  const [time, setTime] = useState(0);
+  const myInterval: React.MutableRefObject<NodeJS.Timer | undefined> = useRef();
+
+  const handleTime = () => {
+    setTime((prevTime) => prevTime + 1);
+  };
+
+  useEffect(() => {
+    myInterval.current = setInterval(handleTime, 1000);
+
+    return () => {
+      clearInterval(myInterval.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (time === 60) {
+      clearInterval(myInterval.current);
+    }
+  }, [time]);
 
   const handleClick = () => {
     dispatch(resetGame());
@@ -16,10 +36,12 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Your score : {points}</h1>
-      <Button onClick={handleClick} buttonStyle={ButtonType.ButtonReset}>
-        Reset
-      </Button>
+      <div className="game-header">
+        <h1>Your score : {points}</h1>
+        <Button onClick={handleClick} buttonStyle={ButtonType.ButtonReset}>
+          Reset
+        </Button>
+      </div>
       <Cards />
     </div>
   );
